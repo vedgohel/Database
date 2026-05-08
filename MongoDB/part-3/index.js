0afe2f87d9063bffbb1a09dcb9d3c4a77aa2e8c2
vedgohel <vedgohel85@gmail.com>
2026-05-08 21:06:45 +0530

    updated file

--- MongoDB/part-3/index.js
+++ MongoDB/part-3/index.js
@@ -35,3 +35,79 @@
 // db.users.find()
 // Output Example
 // { acknowledged: true, deletedCount: 1 }
+
+
+// ved gohel
+
+
+const mongoose = require("mongoose");
+
+// Connect MongoDB
+mongoose.connect("mongodb://localhost:27017/test")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+
+// ================= SCHEMA =================
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+  isActive: Boolean
+});
+
+
+// ================= MODEL =================
+
+const User = mongoose.model("User", userSchema);
+
+
+// ================= DELETE ONE =================
+
+User.deleteOne({ name: "Ved" })
+  .then((res) => {
+    console.log("Delete One:");
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// ================= DELETE MANY =================
+
+User.deleteMany({ isActive: false })
+  .then((res) => {
+    console.log("Delete Many:");
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// ================= FIND BY ID AND DELETE =================
+
+// Replace with real MongoDB _id
+
+User.findByIdAndDelete("665b1a2c4f1d2e1234567890")
+  .then((res) => {
+    console.log("Deleted User:");
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+// ================= FIND ONE AND DELETE =================
+
+User.findOneAndDelete({ name: "Rahul" })
+  .then((res) => {
+    console.log("Find One And Delete:");
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
